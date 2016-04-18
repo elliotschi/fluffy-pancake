317ebc9cdfd307a9a2b6908214cd70876163b7e2
Elliot Chi <elliotschi@gmail.com>
2016-04-17 20:25:18 -0700

    chore: enhanced firebase script

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 'use strict'
 const Firebase = require('firebase');
 const fluffyPancakes = new Firebase('https://fluffy-pancakes.firebaseIO.com/');
-
-const request = require('bluebird').promisify(require('request'));
+const Promise = require('bluebird');
+const request = Promise.promisify(require('request'));
 
 request({
   method: 'POST',
@@ -11,16 +11,39 @@ request({
 .then(function(response, body) {
   let data = JSON.parse(response.body);
   
-  data.forEach((player) => {
-    let playerObj = {
+  // data.forEach((player) => {
+  //   let playerObj = {
+  //     id: player.id,
+  //     player_name: player.player_name
+  //   }
+  //   fluffyPancakes.push(playerObj)
+  //     .then(() => {
+  //       console.log('pushed in player object');
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     })
+  // });
+  Promise.each(data, player => {
+    let playObj = {
       id: player.id,
       player_name: player.player_name
-    }
+    };
+    
     fluffyPancakes.push(playerObj)
       .then(() => {
         console.log('pushed in player object');
       })
-  });
+      .catch(err => {
+        console.error(err);
+      });
+  })
+  .then(() => {
+    console.log('done printing out each player');
+  })
+})
+.catch(err => {
+  console.error(err);
 });
 
 
